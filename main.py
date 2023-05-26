7eecb1e14b33ed6a03885b1cfa33bb897d7b5b13
21satspleb <julius.p91@gmail.com>
2023-05-26 17:21:28 +0200

    Setup project

---
+++ main.py
@@ -0,0 +1,18 @@
+# Import required libraries
+from PIL import Image
+from PIL.ExifTags import TAGS, GPSTAGS
+import os
+import shutil
+import folium
+import streamlit as st
+from shapely.geometry import Point, Polygon
+import geojson
+
+# The main part of the app
+def main():
+    print("test")
+    pass
+
+# Run the app
+if __name__ == "__main__":
+    main()
